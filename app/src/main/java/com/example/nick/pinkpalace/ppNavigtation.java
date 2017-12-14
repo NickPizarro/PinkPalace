5fb784b0267aa338c2146e34e7c868435da7c0c5
nick <pizarron11@gmail.com>
2017-12-13 18:40:30 -0600

    Improved the layout of the youtube button

--- app/src/main/java/com/example/nick/pinkpalace/ppNavigtation.java
+++ app/src/main/java/com/example/nick/pinkpalace/ppNavigtation.java
@@ -58,6 +58,7 @@ public class ppNavigtation extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
 
+
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
